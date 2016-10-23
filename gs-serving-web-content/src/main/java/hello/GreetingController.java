d207698211e9b967dc73f7bf3b7725af755b8e62
filippo.ermini <filippo.ermini@gmail.com>
2016-10-24 00:08:04 +0200

    Aggiunta di angular js per la gestione di richieste rest

--- gs-serving-web-content/src/main/java/hello/GreetingController.java
+++ gs-serving-web-content/src/main/java/hello/GreetingController.java
@@ -1,19 +1,23 @@
 package hello;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class GreetingController {
 
+	private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+    
     @RequestMapping("/greeting")
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        System.out.println("ciao");
-        return "greeting";
+    public Hello greeting(@RequestParam(value="name", required=false, defaultValue="World") String name) {
+    	
+    	return new Hello(counter.incrementAndGet(),String.format(template, name));
         
     }
 
